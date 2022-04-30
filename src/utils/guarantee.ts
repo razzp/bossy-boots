42771579e4b653f8985d3749a5809a31c0c87675
razzp <64423130+razzp@users.noreply.github.com>
2022-04-30 13:00:42 +0100

    Now using correct method in guarantee()

--- src/utils/guarantee.ts
+++ src/utils/guarantee.ts
@@ -1,11 +1,11 @@
-import { assertIsDefined } from '../assertions/assertIsDefined';
+import { assertIsNotNullish } from '../assertions/assertIsNotNullish';
 
 function guarantee<T>(value: T): NonNullable<T>;
 function guarantee<T, U>(value: T, fallback: U): NonNullable<T | U>;
 function guarantee(value: unknown, fallback?: unknown): NonNullable<unknown> {
     const result = value ?? fallback;
 
-    assertIsDefined(result);
+    assertIsNotNullish(result);
 
     return result;
 }
--- tests/utils/guarantee.test.ts
+++ tests/utils/guarantee.test.ts
@@ -1,7 +1,7 @@
 import { AssertionError } from '../../src/internal/AssertionError';
 import { guarantee } from '../../src/utils/guarantee';
 
-test('Given a value that is defined, returns that value', () => {
+test('Given a value that is non-nullish, returns that value', () => {
     expect(guarantee('foo')).toBe('foo');
     expect(guarantee(1)).toBe(1);
     expect(guarantee(true)).toBe(true);
@@ -9,17 +9,19 @@ test('Given a value that is defined, returns that value', () => {
     expect(guarantee(NaN)).toBeNaN();
 });
 
-test('Given a value that is undefined, throws', () => {
+test('Given a value that is nullish, throws', () => {
     expect(() => guarantee(null)).toThrowError(AssertionError);
     expect(() => guarantee(undefined)).toThrowError(AssertionError);
 });
 
-test('Given a value that is undefined, but a fallback that is defined, returns the fallback', () => {
+test('Given a value that is nullish, but a fallback that is non-nullish, returns the fallback', () => {
     expect(guarantee(null, 'foo')).toBe('foo');
-    expect(guarantee(undefined, 1)).toBe(1);
+    expect(guarantee(undefined, 'foo')).toBe('foo');
 });
 
-test('Given a value and fallback that are both undefined, throws', () => {
+test('Given a value and fallback that are both nullish, throws', () => {
     expect(() => guarantee(null, null)).toThrowError(AssertionError);
     expect(() => guarantee(undefined, undefined)).toThrowError(AssertionError);
+    expect(() => guarantee(null, undefined)).toThrowError(AssertionError);
+    expect(() => guarantee(undefined, null)).toThrowError(AssertionError);
 });
