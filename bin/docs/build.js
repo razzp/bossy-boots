3fee5f66c53b216674575e4d0217c3eae1dca4a7
razzp <64423130+razzp@users.noreply.github.com>
2022-05-06 17:28:18 +0100

    Working on automating docs

---
+++ bin/docs/build.js
@@ -0,0 +1,133 @@
+/* eslint-env node */
+
+import { readFile, writeFile } from 'fs/promises';
+import Handlebars from 'handlebars';
+import jsdoc from 'jsdoc-api';
+
+import {
+    formatParamName,
+    formatParamType,
+    getAbstractionGroups,
+    getInstanceNodes,
+    getNodes,
+    getTypeNodes,
+    loadPartials,
+    paramsToString,
+    typeAbstractionTransformer,
+    valueEquals,
+    wrapCode,
+} from './utils.js';
+
+async function run() {
+    // Start a timer.
+    console.time('Finished');
+
+    // Load and register Handlebars partials.
+    for (const { name, template } of await loadPartials()) {
+        Handlebars.registerPartial(name, template);
+    }
+
+    // Register Handlebars helpers.
+    Handlebars.registerHelper('paramsToString', paramsToString);
+    Handlebars.registerHelper('formatParamName', formatParamName);
+    Handlebars.registerHelper('formatParamType', formatParamType);
+    Handlebars.registerHelper('valueEquals', valueEquals);
+
+    // Load and compile the Handlebars template.
+    const render = Handlebars.compile(
+        await readFile('./bin/docs/template.hbs', 'utf8')
+    );
+
+    // Get the documentation data from JSDoc.
+    const docData = await jsdoc.explain({
+        files: './dist/main.js',
+    });
+
+    // Build the model that we'll pass to Handlebars to render.
+    const model = {
+        api: [
+            {
+                name: 'Assertions',
+                methods: getNodes(docData, 'Assertions'),
+                members: [
+                    {
+                        name: 'Instance',
+                        id: 'Assertions.Instance',
+                        methods: getInstanceNodes(docData, 'Assertions'),
+                    },
+                    {
+                        name: 'Type',
+                        id: 'Assertions.Type',
+                        methods: getTypeNodes(docData, 'Assertions'),
+                    },
+                ],
+            },
+            {
+                name: 'Guards',
+                members: [
+                    {
+                        name: 'Instance',
+                        id: 'Guards.Instance',
+                        methods: getInstanceNodes(docData, 'Guards'),
+                    },
+                    {
+                        name: 'Type',
+                        id: 'Guards.Type',
+                        methods: getTypeNodes(docData, 'Guards'),
+                    },
+                ],
+            },
+            {
+                name: 'Common use-case wrappers',
+                id: 'Abstractions',
+                abstractions: [
+                    {
+                        name: 'Instance',
+                        id: 'Abstractions.Instance',
+                        groups: getAbstractionGroups(
+                            getNodes(
+                                docData,
+                                ['Assertions.Instance', 'Guards.Instance'],
+                                /(?:assertIs|is)(?!(?:Not)?InstanceOf)(Not)?(\w+)/
+                            ),
+                            wrapCode
+                        ),
+                    },
+                    {
+                        name: 'Type',
+                        id: 'Abstractions.Type',
+                        groups: getAbstractionGroups(
+                            getNodes(
+                                docData,
+                                ['Assertions.Type', 'Guards.Type'],
+                                /(?:assertIs|is)(?!(?:Not)?TypeOf)(Not)?(\w+)/
+                            ),
+                            typeAbstractionTransformer
+                        ),
+                    },
+                ],
+            },
+            {
+                name: 'Utils',
+                methods: getNodes(docData, 'Utils'),
+            },
+        ],
+    };
+
+    // Write the model to disk so it can be inspected.
+    //await writeFile('./test.json', JSON.stringify(model));
+
+    // Render the docs.
+    const output = render(model);
+
+    // Write the output to disc.
+    await writeFile('./README.md', output);
+
+    // Stop the timer.
+    console.timeEnd('Finished');
+}
+
+run().catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
---
+++ bin/docs/utils.js
@@ -0,0 +1,126 @@
+/* eslint-env node */
+
+import { readFile } from 'fs/promises';
+import glob from 'glob';
+import path from 'path';
+
+function formatParamName(param) {
+    if (param.optional) {
+        return `[${param.name}]`;
+    } else if (param.variable) {
+        return `...${param.name}`;
+    }
+
+    return param.name;
+}
+
+function formatParamType(type) {
+    return type.names.map((name) => `\`${name}\``).join(' \\| ');
+}
+
+function getArrayPos(prefix, not) {
+    switch (prefix) {
+        case 'assertIs':
+            return not ? 1 : 0;
+        default:
+            return not ? 3 : 2;
+    }
+}
+
+function getAbstractionGroups(methods, nameTransformer) {
+    const abstractions = methods.reduce((map, { name }) => {
+        const regexp = /(assertIs|is)(Not)?(\w+)/;
+        const [, prefix, not, modifier] = regexp.exec(name);
+        const arrayPos = getArrayPos(prefix, not);
+        const entry = map.get(modifier);
+
+        if (entry) {
+            entry.methods[arrayPos] = name;
+        } else {
+            map.set(
+                modifier,
+                ((methods) => {
+                    methods[arrayPos] = name;
+
+                    return {
+                        name: modifier,
+                        displayName: nameTransformer?.(modifier) ?? modifier,
+                        methods,
+                    };
+                })(Array(4).fill(undefined))
+            );
+        }
+
+        return map;
+    }, new Map());
+
+    return [...abstractions.values()];
+}
+
+function getNodes(nodes, namespaces, pattern = /.*/) {
+    namespaces = Array.isArray(namespaces) ? namespaces : [namespaces];
+
+    return nodes.filter(
+        (node) => namespaces.includes(node.memberof) && node.name.match(pattern)
+    );
+}
+
+function getInstanceNodes(data, category) {
+    return getNodes(data, `${category}.Instance`, /InstanceOf$/);
+}
+
+function getTypeNodes(data, category) {
+    return getNodes(data, `${category}.Type`, /TypeOf$/);
+}
+
+async function loadPartials() {
+    return Promise.all(
+        glob.sync('./bin/docs/partials/*.hbs').map(async (file) => {
+            const name = path.parse(file).name;
+            const template = await readFile(file, 'utf8');
+
+            return {
+                name,
+                template,
+            };
+        })
+    );
+}
+
+function paramsToString(params) {
+    return params.map(formatParamName).join(', ');
+}
+
+function typeAbstractionTransformer(name) {
+    const nullishSupplement = '(<code>null</code> or <code>undefined</code>)';
+    const nameFormatted = wrapCode(name.toLowerCase());
+
+    switch (name) {
+        case 'Nullish':
+            return `${nameFormatted} <em>${nullishSupplement}</em>`;
+        default:
+            return nameFormatted;
+    }
+}
+
+function valueEquals(value, input) {
+    return value === input;
+}
+
+function wrapCode(input) {
+    return `<code>${input}</code>`;
+}
+
+export {
+    formatParamName,
+    formatParamType,
+    getAbstractionGroups,
+    getNodes,
+    getInstanceNodes,
+    getTypeNodes,
+    loadPartials,
+    paramsToString,
+    typeAbstractionTransformer,
+    valueEquals,
+    wrapCode,
+};
